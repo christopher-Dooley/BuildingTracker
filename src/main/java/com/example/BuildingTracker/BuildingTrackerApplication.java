a64f12f25e71856ab1b427fdc571ec67d6edf8f3
The-Multitud-e <chris_dooley123@hotmail.co.uk>
2024-05-29 16:10:27 +0100

    run spring app

--- src/main/java/com/example/BuildingTracker/BuildingTrackerApplication.java
+++ src/main/java/com/example/BuildingTracker/BuildingTrackerApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BuildingTrackerApplication {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
-//        SpringApplication.run(BuildingTrackerApplication.class, args);
+        SpringApplication.run(BuildingTrackerApplication.class, args);
     }
 
 }

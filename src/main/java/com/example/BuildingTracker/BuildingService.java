0bc72af11e1e194a680586e61d2a571171b55a9e
The-Multitud-e <chris_dooley123@hotmail.co.uk>
2024-05-30 00:13:18 +0100

    Corrections for service methods and new methods

--- src/main/java/com/example/BuildingTracker/BuildingService.java
+++ src/main/java/com/example/BuildingTracker/BuildingService.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 import java.util.UUID;
 
+import static com.example.BuildingTracker.BuildingTransformer.toDto;
+import static com.example.BuildingTracker.BuildingTransformer.toEntity;
+
 @Service
 public class BuildingService {
 
@@ -20,11 +23,36 @@ public class BuildingService {
         return repository.save(building);
     }
 
-    public Optional<BuildingEntity> findBuildingByUUID(UUID uuid) {
-        return repository.findById(uuid);
+    public Optional<BuildingDTO> findBuildingByUUID(UUID uuid) {
+        return repository.findById(uuid).map(BuildingTransformer::toDto);
     }
 
-    public Optional<BuildingEntity> findBuildingByName(String name) {
-        return repository.findByName(name);
+    public Optional<BuildingDTO> findBuildingByName(String name) {
+        return repository.findByName(name).map(BuildingTransformer::toDto);
+    }
+
+    public Optional<BuildingDTO> newBuilding(BuildingDTO dto) {
+        try {
+            fillUuidIfNull(dto);
+            fillCoordsIfNull(dto);
+            BuildingEntity savedEntity = repository.save(toEntity(dto));
+            return Optional.of(toDto(savedEntity));
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+
+    private void fillUuidIfNull(BuildingDTO dto) {
+        if (dto.getUuid() == null) {
+            dto.setUuid(UUID.randomUUID());
+        }
+    }
+
+    private void fillCoordsIfNull(BuildingDTO dto) {
+        if (dto.getLongitude() == null ||  dto.getLatitude() == null) {
+            //TODO add coord request here
+            dto.setLongitude(1);
+            dto.setLatitude(1);
+        }
     }
 }

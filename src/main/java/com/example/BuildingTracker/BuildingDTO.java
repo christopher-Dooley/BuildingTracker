02c9891fca3405e2b68c2d6681e5818858c48daa
The-Multitud-e <chris_dooley123@hotmail.co.uk>
2024-05-29 19:41:27 +0100

    DTO class

---
+++ src/main/java/com/example/BuildingTracker/BuildingDTO.java
@@ -0,0 +1,140 @@
+package com.example.BuildingTracker;
+
+import java.util.Objects;
+
+public class BuildingDTO {
+
+    private String name;
+    private String street;
+    private String number;
+    private String postCode;
+    private String city;
+    private String country;
+    private String description;
+    private double longitude;
+    private double latitude;
+
+    public BuildingDTO(String name, String street, String number, String postCode, String city, String country, String description, float longitude, float latitude) {
+        this.name = name;
+        this.street = street;
+        this.number = number;
+        this.postCode = postCode;
+        this.city = city;
+        this.country = country;
+        this.description = description;
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    public BuildingDTO(String name, String street, String number, String postCode, String city, String country, String description) {
+        this.name = name;
+        this.street = street;
+        this.number = number;
+        this.postCode = postCode;
+        this.city = city;
+        this.country = country;
+        this.description = description;
+        this.longitude = Float.NaN;
+        this.latitude = Float.NaN;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BuildingDTO that = (BuildingDTO) o;
+        return Double.compare(longitude, that.longitude) == 0 && Double.compare(latitude, that.latitude) == 0 && Objects.equals(name, that.name) && Objects.equals(street, that.street) && Objects.equals(number, that.number) && Objects.equals(postCode, that.postCode) && Objects.equals(city, that.city) && Objects.equals(country, that.country) && Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, street, number, postCode, city, country, description, longitude, latitude);
+    }
+
+    @Override
+    public String toString() {
+        return "BuildingDTO{" +
+                "name='" + name + '\'' +
+                ", street='" + street + '\'' +
+                ", number='" + number + '\'' +
+                ", postCode='" + postCode + '\'' +
+                ", city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                ", description='" + description + '\'' +
+                ", longitude=" + longitude +
+                ", latitude=" + latitude +
+                '}';
+    }
+}

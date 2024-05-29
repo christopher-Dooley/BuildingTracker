fd67f58247652497e4470dece1c1eab6b52ae686
The-Multitud-e <chris_dooley123@hotmail.co.uk>
2024-05-29 18:43:03 +0100

    building repository class

---
+++ src/main/java/com/example/BuildingTracker/BuildingRepository.java
@@ -0,0 +1,15 @@
+package com.example.BuildingTracker;
+
+import org.springframework.data.repository.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface BuildingRepository extends Repository<BuildingEntity, UUID> {
+
+    BuildingEntity save(BuildingEntity building);
+
+    Optional<BuildingEntity> findById(UUID uuid);
+
+    Optional<BuildingEntity> findByName(String name);
+}

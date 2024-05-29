b22d9b81556d082f2d0e7b10244c53a518ecf2db
The-Multitud-e <chris_dooley123@hotmail.co.uk>
2024-05-29 16:06:26 +0100

    hello world

---
+++ src/main/java/com/example/BuildingTracker/BuildingTrackerApplication.java
@@ -0,0 +1,14 @@
+package com.example.BuildingTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingTrackerApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+//        SpringApplication.run(BuildingTrackerApplication.class, args);
+    }
+
+}

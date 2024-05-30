e9e38a4bb7b9dce28f4f48a759bba6404e9eea31
The-Multitud-e <chris_dooley123@hotmail.co.uk>
2024-05-30 17:46:57 +0100

    changing service method + some renaming

--- src/main/java/com/example/BuildingTracker/BuildingService.java
+++ src/main/java/com/example/BuildingTracker/BuildingService.java
@@ -1,5 +1,7 @@
 package com.example.BuildingTracker;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,15 +14,14 @@ import static com.example.BuildingTracker.BuildingTransformer.toEntity;
 @Service
 public class BuildingService {
 
+    private static final Logger log = LoggerFactory.getLogger(BuildingService.class);
     private final BuildingRepository repository;
+    private final CoordinateRequestService coordinateRequestService;
 
     @Autowired
-    public BuildingService(BuildingRepository repository) {
+    public BuildingService(BuildingRepository repository, CoordinateRequestService coordinateRequestService) {
         this.repository = repository;
-    }
-
-    public BuildingEntity saveBuilding(BuildingEntity building) {
-        return repository.save(building);
+        this.coordinateRequestService = coordinateRequestService;
     }
 
     public Optional<BuildingDTO> findBuildingByUUID(UUID uuid) {
@@ -31,28 +32,27 @@ public class BuildingService {
         return repository.findByName(name).map(BuildingTransformer::toDto);
     }
 
-    public Optional<BuildingDTO> newBuilding(BuildingDTO dto) {
+    public Optional<BuildingDTO> newBuilding(BuildingDTO dto) throws CoordinateRequestException {
         try {
-            fillUuidIfNull(dto);
-            fillCoordsIfNull(dto);
+            setUuidIfNull(dto);
+            setCoordinatesIfNull(dto);
             BuildingEntity savedEntity = repository.save(toEntity(dto));
             return Optional.of(toDto(savedEntity));
         } catch (Exception e) {
-            return Optional.empty();
+            log.error("Saving new building failed", e);
+            throw e;
         }
     }
 
-    private void fillUuidIfNull(BuildingDTO dto) {
+    private void setUuidIfNull(BuildingDTO dto) {
         if (dto.getUuid() == null) {
             dto.setUuid(UUID.randomUUID());
         }
     }
 
-    private void fillCoordsIfNull(BuildingDTO dto) {
+    private void setCoordinatesIfNull(BuildingDTO dto) throws CoordinateRequestException {
         if (dto.getLongitude() == null ||  dto.getLatitude() == null) {
-            //TODO add coord request here
-            dto.setLongitude(1);
-            dto.setLatitude(1);
+            coordinateRequestService.setCoordinatesForBuilding(dto);
         }
     }
 }
